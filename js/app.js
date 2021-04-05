bbb29e5161857ebe7883b7f2916cb5074d1c4db3
mmergal92 <mmergal92@gmail.com>
2021-04-04 23:13:36 -0400

    added code to remove images of one artist when you click on the next one

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
 //Met Museum API
 const baseURL = `https://collectionapi.metmuseum.org/public/collection/v1/objects/`
+//Created Arrays from the Object IDs of works of arts from certain artists from the Search API of the Met Museum
 const objectVanIdArray = [459123, 436533, 437984, 436535, 436528, 436529, 436532, 459193, 436531, 437998];
 let currentId = 459123;
-const objectCezIDarray = [435876, 435882, 435885, 435868, 435877, 435872, 437990, 459092, 437989, 435875];
-
-const $secondButton = $(".second")
+const objectCezIdArray = [435876, 435882, 435885, 435868, 435877, 435872, 437990, 459092, 437989, 435875];
+const objectManIdArray = [438002, 436965, 436964, 436953, 436944, 436960, 436950, 436947, 436951, 436952];
+const objectDegIdArray = [436155, 438817, 436157, 436121, 436144, 436126, 436141, 436162, 436135, 436122];
+const objectSeuIdArray = [437654, 437658, 459191, 459116, 437655, 339751, 437656, 437659, 438123, 437657];
 
 const getData = () => {
+    const $container = $('<div class = items>')
+    $('.items').remove()
     // console.log("this works")
     $.ajax({
         url: baseURL + currentId,
@@ -14,7 +18,6 @@ const getData = () => {
         }).then(function(data){
     //   alert("Retrieved records from the dataset!");
       console.log(data);
-      const $container = $('<div>')
         $container.html(`
         <h3> ${data.artistDisplayName} </h3>
         <h4> ${data.artistNationality + " " + data.artistRole}</h4>
@@ -23,22 +26,65 @@ const getData = () => {
         const $img = $('<img>').attr('src', data.primaryImage)
         $img.addClass('image')
         $container.append($img)
-        $('.large-container').append($container)
+        const $largecontainer = $('.large-container')
+        $largecontainer.append($container)
         // console.log(data.primaryImage);
         },(error) =>{
             console.log(error);
 })
 }
 
+
 $(()=>{
-    const $firstButton = $(".first")
+const $firstButton = $(".first")
+const $secondButton = $(".second")
+const $thirdButton = $(".third")
+const $fourthButton = $(".fourth")
+const $fifthButton = $(".fifth")
+
     const artistVan = (event) => {
         console.log ("it works")
-        event.preventDefault()
+        event.preventDefault();
     for (let i=0; i <objectVanIdArray.length; i++){
         currentId = objectVanIdArray[i];
         getData();
     }
     }
+    const artistCez = (event) => {
+        console.log ("it works")
+        event.preventDefault();
+    for (let i=0; i <objectCezIdArray.length; i++){
+        currentId = objectCezIdArray[i];
+        getData()
+    }
+    }
+    const artistMan = (event) => {
+        console.log ("it works")
+        event.preventDefault();
+    for (let i=0; i <objectManIdArray.length; i++){
+        currentId = objectManIdArray[i];
+        getData();
+    }
+    }
+    const artistDeg = (event) => {
+        console.log ("it works")
+        event.preventDefault();
+    for (let i=0; i <objectDegIdArray.length; i++){
+        currentId = objectDegIdArray[i];
+        getData();
+    }
+    }
+    const artistSeu = (event) => {
+        console.log ("it works")
+        event.preventDefault();
+    for (let i=0; i <objectSeuIdArray.length; i++){
+        currentId = objectSeuIdArray[i];
+        getData();
+    }
+    }   
 $firstButton.click(artistVan);
+$secondButton.click(artistCez);
+$thirdButton.click(artistMan);
+$fourthButton.click(artistDeg);
+$fifthButton.click(artistSeu);
 });
